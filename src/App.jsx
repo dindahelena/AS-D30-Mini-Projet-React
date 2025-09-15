1a5e391802013837d49352f116aff2a272b3052a
dindahelena <dindahelenaputri@gmail.com>
2025-09-15 17:58:15 +0900

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,30 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import UserDetailPage from "./pages/UserDetailPage";
+import ProtectedRoute from "./routes/ProtectedRoute";
+import { AuthProvider } from "./services/Api";
 
+export default function App() {
+  return (
+    <AuthProvider>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route
+            path="/user/:id"
+            element={
+              <ProtectedRoute>
+                <UserDetailPage />
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
+  );
+}
